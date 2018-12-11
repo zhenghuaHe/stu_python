1128e5acde3931eed54aee7730421582993e9e89
zhenghuahe <hezhenghua@mwteck.com>
2018-12-11 15:56:13 +0800

    2018-12-10

--- python3/read_log.py
+++ python3/read_log.py
@@ -10,43 +10,45 @@ from email.utils import parseaddr, formataddr
 
 import smtplib
 
-def _format_add(s):
-    name, addr = parseaddr(s)
-    return formataddr((Header(name, "utf-8").encode(), addr))
-
 from_addr = 'mwteck@mwteck.com'
 password = 'Uvz66331McG9FTjcTUN0'
 to_addr = 'hezhenghua@mwteck.com'
 smtp_server = 'smtp.exmail.qq.com'
 
-# msg = MIMEText('hello.send by python', 'plain', 'utf-8')
-# msg['From'] = _format_add(from_addr)
-# msg['To'] = _format_add(to_addr)
-# msg['Subject'] = Header('异常响应码日志.....', 'utf-8').encode()
 
-# server = smtplib.SMTP(smtp_server,25)
-# server.set_debuglevel(1)
-# server.login(from_addr, password)
-# server.sendmail(from_addr, [to_addr], msg.as_string())
-# server.quit()
+def _format_add(s):
+    name, addr = parseaddr(str(s))
+    msg = MIMEText(str(s), 'plain', 'utf-8')
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = Header('异常响应码日志.....', 'utf-8').encode()
+
+    server = smtplib.SMTP(smtp_server,25)
+    server.set_debuglevel(1)
+    server.login(from_addr, password)
+    server.sendmail(from_addr, [to_addr], msg.as_string())
+    server.quit()
+
+    return formataddr((Header(name, "utf-8").encode(), addr))
+
 
 error_status = {"404","500","502"}
 
-logfile = open("python-api.t5.2012iot.com.log")
-line = json.loads(logfile.readline())
-print(line["@fields"])
-while line:
-    print("request:", line["@fields"]["request"])
-    print("status:", line["@fields"]["status"])
-    status = line["@fields"]["status"]
-    print("request_time:", line["@fields"]["request_time"])
-    line = logfile.readline()
-
-    if status in error_status:
-        pass
+with open('python-api.t5.2012iot.com.log', 'r+') as f:
+    while True:
+        line = f.readline()
+        if line:
+            load_data = json.loads(line)
+            print(load_data)
+            print(load_data["@fields"]['request'])
+            print("request:", load_data["@fields"]["request"])
+            print("status:", load_data["@fields"]["status"])
+            print("request_time:", load_data["@fields"]["request_time"])
 
 
-logfile.close()
+            status = load_data["@fields"]["status"]
+            if status in error_status:
+                _format_add(load_data["@fields"])
 
 
 
